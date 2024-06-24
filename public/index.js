9b6627397b74a3efb337e5f43ec685fb76afb6de
MainKronos <lorenzo.chesi@live.it>
2024-06-24 19:01:25 +0200

    Update

--- public/index.js
+++ public/index.js
@@ -33,7 +33,5 @@ form.addEventListener("submit", async (event) => {
 
   const url = search(address.value, searchEngine.value);
 
-  let frame = document.getElementById("uv-frame");
-  frame.style.display = "block";
-  frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+  window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
